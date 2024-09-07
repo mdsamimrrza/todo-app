be3b30d474859e86923ec7b435ff32178a8f60ff
Md Samim Reza <mdsamimrrza@gmail.com>
2024-09-07 11:44:50 +0530

    color changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
+  const[color ,setColor] = useState('olive')
+ 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className='w-full h-screen duration-200 ' style={{backgroundColor:color}}>
+        <div className='fixed flex flex-wrap justify-center bottom-12 px-2 insert-x-0'>
+          <div className='flex flex-warp justify-center shadow-lg bg-white  mr-3  px-3 py-2 rounded-2xl'>
+          <button
+          onClick={()=>setColor('red')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg'
+          style={{backgroundColor:'red'}}
+          >red</button>
+          <button
+           onClick={()=>setColor('blue')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg'
+          style={{backgroundColor:'blue'}}
+          >blue</button>
+          <button
+           onClick={()=>setColor('green')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg'
+          style={{backgroundColor:'green'}}
+          >gren</button>
+          <button
+           onClick={()=>setColor('yellow')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg'
+          style={{backgroundColor:'yellow'}}
+          >yellow</button>
+          <button
+           onClick={()=>setColor('orange')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg'
+          style={{backgroundColor:'orange'}}
+          >orange</button>
+            <button
+           onClick={()=>setColor('grey')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg'
+          style={{backgroundColor:'grey'}}
+          >grey</button>
+
+          </div>
+        </div>
+     </div>
     </>
   )
 }
-
-export default App
